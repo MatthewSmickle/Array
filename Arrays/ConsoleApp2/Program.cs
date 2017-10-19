d0b1d14311ba009d8e3dcf279e71974068c7d746
MatthewSmickle <matt.smickle.o@gmail.com>
2017-10-19 12:30:43 -0400

    good shit

---
+++ Arrays/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Boolean dupe = false;
+            int[] nums = new int[50];
+            Random rnd = new Random();
+            for(int i = 0;i < nums.Length; i ++)
+            {
+                dupe = false;
+                int ran = rnd.Next(0, 51);
+                for (int j = 0; j < nums.Length; j++)
+                {
+                     
+                    if (nums[j] == ran)
+                    {
+                        dupe = true; 
+                    }
+                    
+                }
+
+                if(dupe != true)
+                {
+                    nums[i] = ran;
+                }
+               
+                
+            }
+            
+            for(int i = 0; i <50; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+               
+            }
+        }
+    }
+}
---
+++ Arrays/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5000];
+            Random rnd = new Random();
+            int greatest1 = 0;
+            int greatest2 = 0;
+            for(int i = 0; i<5000; i++)
+            {
+                nums[i] = rnd.Next(0, 1000001);
+            }
+
+            for(int i = 0; i< )
+
+        }
+    }
+}
